7141bbf0bbf544fce17ee3e9e5f58e6e110c8e44
coder <codenow8@gmail.com>
2023-07-14 19:50:40 +0545

    footer & navbar with loading Components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+   <>
+    <div>
+    <div>Copyright © 2012 - 2023 Wasabi Dev®. All rights reserved.</div>
+    <div></div>
+    </div>
+   </>
+  )
+}
+
+export default Footer
