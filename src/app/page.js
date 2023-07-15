e69b27ffb0f1758660b5661ce79fe321ef00ff88
coder <codenow8@gmail.com>
2023-07-15 09:34:02 +0545

    next js image tag

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
 export default function Home() {
   return (
     <>
       <div>
-        Home
+        <Image src={Hero} alt="hero image" className={styles.hero} />
       </div>
     </>
-  )
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
-import styles from "./footer.module.css"
+import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-   <>
-    <div className={styles.footer}>
-    <div>Copyright © 2012 - 2023 Wasabi Dev®. All rights reserved.</div>
-    <div></div>
-    </div>
-   </>
-  )
-}
+    <>
+      <div className={styles.footer}>
+        <div>
+          <p className={styles.text}>
+            Copyrights © 2023 Wasabi Dev® All rights reserved.
+          </p>
+        </div>
+        <div className={styles.social}>
+          <Image
+            src="/1.png"
+            width="15"
+            className={styles.social_img}
+            height="15"
+            alt="facebook"
+          />
+          <Image
+            src="/2.png"
+            width="15"
+            className={styles.social_img}
+            height="15"
+            alt="instagram"
+          />
+          <Image
+            src="/3.png"
+            width="15"
+            className={styles.social_img}
+            height="15"
+            alt="twitter"
+          />
+          <Image
+            src="/4.png"
+            width="15"
+            className={styles.social_img}
+            height="15"
+            alt="youtube"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
