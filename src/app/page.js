2c83af6887b69a177e2d6d52eef35e15dc3346c8
coder <codenow8@gmail.com>
2023-07-15 13:08:11 +0545

    Home Hero Section Added

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,19 @@ import Hero from "public/hero.png";
 export default function Home() {
   return (
     <>
-      <div>
-        <Image src={Hero} alt="hero image" className={styles.hero} />
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>
+           Better Design For Your Digital Products.
+          </h1>
+          <p className={styles.description}>
+            Turning your idea into reality.We bring together the teams from global tech industry. 
+          </p>
+          <button className={styles.button}>Explore More</button>
+        </div>
+        <div className={styles.item}>
+          <Image src={Hero} alt="hero image" className={styles.hero_Image} />
+        </div>
       </div>
     </>
   );
