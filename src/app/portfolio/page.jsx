d14be3ab256958c508ca2c127128e567e53da744
coder <codenow8@gmail.com>
2023-07-16 14:58:34 +0545

    portoflio section added

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,13 +1,27 @@
 // "use client"
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const Portfolio = () => {
 
   return (
     
    <>
-     <div className={styles.container}>Portfolio</div>
+     <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.link}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/illustrations" className={styles.link}>
+          <span className={styles.title}>Website</span>
+        </Link>
+        <Link href="/portfolio/illustrations" className={styles.link}>
+          <span className={styles.title}>Applications</span>
+        </Link>
+      </div>
+     </div>
    </>
   )
 }
