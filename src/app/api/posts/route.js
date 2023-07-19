bf38c25eecf73b1635055baa49a72f605183201f
coder <codenow8@gmail.com>
2023-07-19 16:33:05 +0545

    mongodb atlas connection error fixed

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -2,18 +2,28 @@ import { NextResponse } from "next/server";
 import dbConnection from "@/utils/db";
 import Post from "@/models/Post";
 
-
 export const GET = async (request) => {
   try {
     await dbConnection();
 
     const posts = await Post.find();
-
-    return new NextResponse(JSON.stringify(posts), { status: 200 });
-  } catch (err) {
-    return new NextResponse("Database Error", { status: 500 });
+    return new NextResponse(posts, { status: 200 });
+  } catch (error) {
+    return new NextResponse("Database error...", { status: 500 });
   }
 };
+
+// export const GET = async (request) => {
+//   try {
+//     await dbConnection();
+
+//     const posts = await Post.find();
+
+//     return new NextResponse(JSON.stringify(posts), { status: 200 });
+//   } catch (err) {
+//     return new NextResponse("Database Error", { status: 500 });
+//   }
+// };
 
 export async function POST(request) {
   const { title, desc, content, username, img } = await request.json();
--- src/utils/db.js
+++ src/utils/db.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   try {
-    mongoose.set('strictQuery', false)
-    await mongoose.connect(process.env.MONGO);
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
+    console.error("MongoDB Connection Error:", error.message);
     throw new Error("Connection failed!");
   }
 };
