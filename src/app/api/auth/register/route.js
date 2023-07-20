ae344026925765259c040933b64a4183d828cc8f
coder <codenow8@gmail.com>
2023-07-20 14:33:01 +0545

    user registraion success

---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,27 @@
+import User from "@/models/User";
+import dbConnection from "@/utils/db";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export const POST = async (request) => {
+  const { name, email, password } = await request.json();
+
+  await dbConnection();
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    return new NextResponse("User created successfully", {
+      status: 201,
+    });
+  } catch (error) {
+    return new NextResponse(err.message, {
+      status: 500,
+    });
+  }
+};
--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,12 +1,29 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
-  const handleSubmit = (e) => {
+  const [error, setError] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("submit");
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, password }),
+      });
+      res.status === 201 && router.push("/dashboard/login?success=account has been created");
+    } catch (error) {
+      setError(true);
+    }
   };
 
   return (
@@ -33,6 +50,7 @@ const Register = () => {
 
         <button className={styles.button}>Register</button>
       </form>
+      {error && <p className={styles.error}>Something went wrong</p>}
       <Link href="/dashboard/login">Login with existing account</Link>
     </div>
   );
