b4d2cfe3a4fa84e14cdf1780eef1bc955faf175b
coder <codenow8@gmail.com>
2023-07-26 09:44:03 +0545

    delete post function

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -49,8 +49,20 @@ const Dashboard = () => {
           }),
         });
         mutate();
+        e.target.reset();
       } catch (err) {
         console.log(err);
+      }
+    };
+
+    const deletePost = async (id) => {
+      try {
+        await fetch(`/api/posts/${id}`, {
+          method: "DELETE",
+        });
+        mutate();
+      } catch (error) {
+        console.log(error);
       }
     };
     return (
@@ -70,23 +82,28 @@ const Dashboard = () => {
                       />
                     </div>
                     <h2 className={styles.postTitle}>{post.title}</h2>
-                    <span className={styles.delete}>x</span>
+                    <span
+                      className={styles.delete}
+                      onClick={() => deletePost(post._id)}
+                    >
+                      x
+                    </span>
                   </div>
                 ))}
           </div>
-<form className={styles.new} onSubmit={handleSubmit}>
-          <h1>Add New Post</h1>
-          <input type="text" placeholder="Title" className={styles.input} />
-          <input type="text" placeholder="Desc" className={styles.input} />
-          <input type="text" placeholder="Image" className={styles.input} />
-          <textarea
-            placeholder="Content"
-            className={styles.textArea}
-            cols="30"
-            rows="10"
-          ></textarea>
-          <button className={styles.button}>Send</button>
-        </form>
+          <form className={styles.new} onSubmit={handleSubmit}>
+            <h1>Add New Post</h1>
+            <input type="text" placeholder="Title" className={styles.input} />
+            <input type="text" placeholder="Desc" className={styles.input} />
+            <input type="text" placeholder="Image" className={styles.input} />
+            <textarea
+              placeholder="Content"
+              className={styles.textArea}
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button className={styles.button}>Send</button>
+          </form>
         </div>
       </>
     );
