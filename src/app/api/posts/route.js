c24e9951e4dfafb5dc1171375a2c8553a51f320f
coder <codenow8@gmail.com>
2023-07-26 07:55:11 +0545

    post method success

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -3,22 +3,31 @@ import dbConnection from "@/utils/db";
 import Post from "@/models/Post";
 
 export const GET = async (request) => {
+  const url = new URL(request.url);
+  const username = url.searchParams.get("username");
+
   try {
     await dbConnection();
 
-    const posts = await Post.find();
+    const posts = await Post.find(username && { username });
     return new NextResponse(JSON.stringify(posts), { status: 200 });
   } catch (error) {
     return new NextResponse("Database error...", { status: 500 });
   }
 };
 
-export async function POST(request) {
-  const { title, desc, content, username, img } = await request.json();
-  await dbConnection();
-  await Post.create({ title, desc, content, username, img });
-  return NextResponse.json(
-    { message: "Post created successfully" },
-    { status: 200 }
-  );
-}
+export const POST = async (request) => {
+  const body = await request.json();
+
+  const newPost = new Post(body);
+
+  try {
+    await dbConnection();
+
+    await newPost.save();
+
+    return new NextResponse("Post has been created", { status: 201 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,20 +4,22 @@ import styles from "./page.module.css";
 import useSWR from "swr";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Dashboard = () => {
-  // const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  // const { data, error, isLoading } = useSWR(
-  //   "https://jsonplaceholder.typicode.com/posts",
-  //   fetcher
-  // );
-
-  // if (error) return <div>failed to load</div>;
-  // if (isLoading) return <div>loading...</div>;
-  // console.log(data);
-
   const session = useSession();
   const router = useRouter();
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  console.log(data);
+
   console.log(session);
 
   if (session.status === "loading") {
@@ -28,9 +30,63 @@ const Dashboard = () => {
   }
 
   if (session.status === "authenticated") {
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const title = e.target[0].value;
+      const desc = e.target[1].value;
+      const img = e.target[2].value;
+      const content = e.target[3].value;
+
+      try {
+        await fetch("/api/posts", {
+          method: "POST",
+          body: JSON.stringify({
+            title,
+            desc,
+            img,
+            content,
+            username: session.data.user.name,
+          }),
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
     return (
       <>
-        <div className={styles.container}>Dashboard Coming Soon</div>
+        <div className={styles.container}>
+          <div className={styles.posts}>
+            {isLoading
+              ? "Loading...."
+              : data?.map((post) => (
+                  <div className={styles.post} key={post.id}>
+                    <div className={styles.imgCOntainer}>
+                      <Image
+                        src={post.img}
+                        alt="img"
+                        width={200}
+                        height={200}
+                      />
+                    </div>
+                    <h2 className={styles.postTitle}>{post.title}</h2>
+                    <span className={styles.delete}>x</span>
+                  </div>
+                ))}
+          </div>
+<form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input type="text" placeholder="Desc" className={styles.input} />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea
+            placeholder="Content"
+            className={styles.textArea}
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+        </div>
       </>
     );
   }
